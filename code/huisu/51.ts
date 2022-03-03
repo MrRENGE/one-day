040d8ff793f162eed7daec2575abf20321123dd8
renyunhong <renyunhong@didichuxing.com>
2022-03-03 08:33:46 +0800

    N 皇后问题

--- code/huisu/51.ts
+++ code/huisu/51.ts
@@ -2,6 +2,7 @@ function solveNQueens(n: number): string[][] {
   let res: string[][][] = [];
   let board: string[][] = [];
 
+  // 初始化棋盘
   for (let i = 0; i < n; i++) {
     let a = [];
     a.length = n;
@@ -10,6 +11,7 @@ function solveNQueens(n: number): string[][] {
   }
 
   function backtrack(board: string[][], row: number) {
+    // 当前棋盘布局完成，压入 res 中 退出
     if (row === board.length) {
       res.push(JSON.parse(JSON.stringify(board)));
       return;
@@ -31,7 +33,7 @@ function solveNQueens(n: number): string[][] {
   }
 
   function isValid(board: string[][], row: number, col: number): boolean {
-    // 检查顶部是否存在皇后
+    // 检查row之前的所有col列是否存在皇后
     for (let i = 0; i < row; i++) {
       if (board[i][col] === 'Q') {
         return false
@@ -39,7 +41,6 @@ function solveNQueens(n: number): string[][] {
     }
 
     // 检测右上方是否存在皇后
-
     for (let i = row - 1, j = col + 1; i >= 0 && j >= 0; i--, j++) {
       if (board[i][j] === 'Q') {
         return false;
