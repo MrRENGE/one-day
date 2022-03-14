d3025611f3624c5153080ea8576eb72b43de49a7
renyunhong <renyunhong@didichuxing.com>
2022-03-14 23:13:59 +0800

    feat: BST 插入节点

---
+++ code/BFS/701.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+ function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+
+  function dp(root: TreeNode | null, val: number): TreeNode {
+    if (root === null) {
+      return { val, left: null, right: null };
+    }
+
+    if (root.val === val) {
+      return root;
+    }
+
+    if (root.val < val) {
+      root.right = dp(root.right, val);
+    }
+
+    if (root.val > val) {
+      root.left = dp(root.left, val)
+    }
+
+  }
+
+  dp(root, val);
+
+  return root;
+};
